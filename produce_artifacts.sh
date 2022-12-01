e251ec3dd6b9ff2a5c33ffa611ecce64ba42c8b5
Scott Opell <me@scottopell.com>
2022-12-01 23:27:16 +0000

    corrects aarch64 -> arm64 when necessary

--- produce_artifacts.sh
+++ produce_artifacts.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 arch=$(uname -m)
+if [[ "$arch" == "aarch64" ]]; then
+    arch="arm64"
+fi
 plat=$(echo $(uname -s) | awk '{print tolower($0)}')
 
 CARGO_TARGET_DIR="deps/${plat}_${arch}" cargo build --release
